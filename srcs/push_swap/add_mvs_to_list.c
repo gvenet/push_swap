6acf6c54c14de796133e69ee5f780ff79e15da98
gvenet <venetguillaume@gmail.com>
2021-05-06 09:42:06 +0200

    1

--- srcs/push_swap/add_mvs_to_list.c
+++ srcs/push_swap/add_mvs_to_list.c
@@ -11,7 +11,7 @@ void	add_mvs_to_list(t_ps *ps, int ***st, int mvs)
 	if (!tmp)
 	{
 		free_lst(&ps->lst_mvs);
-		exit_clear(stacks, "Error: malloc failed", 1, NULL);
+		exit_clear(stacks, "Error: malloc failed", 1, ps->bloc_data);
 	}
 	(*ps->t->fct_mvs[mvs])(ps->st_size, &stacks);
 	ft_lstadd_back(&ps->lst_mvs, tmp);
